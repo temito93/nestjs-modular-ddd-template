7e9c8565c7f330ee02e15677215978d8239653fb
temojincharadze <temotemo161@gmail.com>
2026-03-24 04:43:02 +0400

    chore: update translations

--- src/i18n/ge/errors.ts
+++ src/i18n/ge/errors.ts
@@ -1,13 +1,13 @@
 import type { ErrorCode } from '@app/common/exceptions';
 
 const translations: Record<ErrorCode, string> = {
-  ORDER_CREATE_FAILED: 'შეკვეთის შექმნა ვერ მოხერხდა',
+  ORDER_CREATE_FAILED: 'ორდერი ვერ შეიქმნა',
   ORDER_NOT_FOUND: 'შეკვეთა ვერ მოიძებნა',
   WALLET_INSUFFICIENT_FUNDS: 'არასაკმარისი ბალანსი',
   WALLET_DEDUCTION_FAILED: 'ბალანსის ჩამოკლება ვერ მოხერხდა',
-  WALLET_UNKNOWN: 'საფულის სერვისი არ პასუხობს',
+  WALLET_UNKNOWN: 'საფულე ვერ მოიძებნა',
   VALIDATION_FAILED: 'ვალიდაციის შეცდომა',
-  KYT_CHECK_FAILED: 'KYT ვალიდაცია ჩაიჭრა',
+  KYT_CHECK_FAILED: 'KYT ვალიდაცია წარუმატებელია',
   INTERNAL_SERVER_ERROR: 'სერვერის შეცდომა',
 };
 
